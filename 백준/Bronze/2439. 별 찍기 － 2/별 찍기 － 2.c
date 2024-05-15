64aabbf053d10e7d6c0672e6c9af11d7b8f47702
teamplay <67694293+hexter31376@users.noreply.github.com>
2024-05-16 03:13:21 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    
+    for(int i = 1; i <= N; i++){
+        for(int k = i; k < N; k++){
+            printf(" ");
+        }
+        for(int j = N - i; j < N; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

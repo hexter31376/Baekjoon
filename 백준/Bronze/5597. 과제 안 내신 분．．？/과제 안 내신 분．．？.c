ba58b5ec0a3e290a7ba12e2c8fb3080a99721e41
teamplay <67694293+hexter31376@users.noreply.github.com>
2024-05-18 00:33:11 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int arr[30];
+    for(int i = 0; i < 30; i++){
+        arr[i] = 0;
+    }
+
+    int input;
+    for(int i = 0; i < 28; i++){
+        scanf("%d", &input);
+        arr[input - 1] = 1;
+    }
+    
+    for(int i = 0; i < 30; i++){
+        if(arr[i] == 0){
+            printf("%d\n", i + 1);
+        }
+    }
+    return 0;
+}

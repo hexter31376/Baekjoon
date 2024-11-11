df11c8b0ded992a39821f1e0b9a855eab630e300
teamplay <67694293+hexter31376@users.noreply.github.com>
2024-11-11 14:13:06 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,13 @@
+# include <iostream>
+# include <algorithm>
+# include <string>
+using namespace std;
+
+int main () {
+    string input;
+    for (int i = 0; i < 100; i++) {
+        getline(cin, input);
+        cout << input << endl;
+    }
+    return 0;
+}
